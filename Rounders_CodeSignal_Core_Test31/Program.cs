183f28a17e61f1aa7b18de8ef38da48c53708dda
Albert Hakobyan <abhakobyan24@gmail.com>
2021-09-08 14:18:00 +0400

    Добавьте файлы проекта.

---
+++ Rounders_CodeSignal_Core_Test31/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rounders_CodeSignal_Core_Test31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input number");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(rounders(n));
+        }
+        static int rounders(int n)
+        {
+            if (n < 10) return n;
+            return rounders(n / 10 + (n % 10 >= 5 ? 1 : 0)) * 10;
+        }
+
+    }
+}
